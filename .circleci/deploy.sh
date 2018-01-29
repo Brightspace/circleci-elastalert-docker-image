49114be54d642dbd27d1f34c46ed74579637ba61
Jeff Ashton <JeffAshton@users.noreply.github.com>
2018-01-29 12:30:05 -0500

    Better tags

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,5 +3,12 @@ set -eu
 
 docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 
-docker tag "$DOCKER_IMAGE_NAME" "$DOCKER_IMAGE_NAME-$CIRCLE_BUILD_NUM"
-docker push "$DOCKER_IMAGE_NAME-$CIRCLE_BUILD_NUM"
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+    buildTag="build"
+else
+    buildTag="rc"
+fi
+
+publishImageName="${DOCKER_IMAGE_NAME}-${buildTag}${CIRCLE_BUILD_NUM}"
+docker tag "$DOCKER_IMAGE_NAME" $publishImageName
+docker push "$publishImageName"
