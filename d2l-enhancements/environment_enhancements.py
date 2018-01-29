6ebbc21c7b18d1220701b631437b03a4a44f6635
Jeff Ashton <JeffAshton@users.noreply.github.com>
2018-01-29 12:49:01 -0500

    Using actual enhancements

--- d2l-enhancements/environment_enhancements.py
+++ d2l-enhancements/environment_enhancements.py
@@ -1,10 +1,18 @@
 from elastalert.enhancements import BaseEnhancement
+from elastalert.enhancements import DropMatchException
 from os import environ
 
 class AppendDataCenter(BaseEnhancement):
+
 	def process(self, match):
-		match['d2l_data_center'] = 'test01'
+		if 'D2L_DATA_CENTER' in environ:
+			match['d2l_data_center'] = environ['D2L_DATA_CENTER']
+		else
+			match['d2l_data_center'] = 'unknown'
 
 class ExcludeDevelopmentEnvironments(BaseEnhancement):
+
 	def process(self, match):
-		pass
+		if 'D2L_DEVELOPMENT_ENVIRONMENT' in environ:
+			if environ['D2L_DEVELOPMENT_ENVIRONMENT'] == 'True':
+				raise DropMatchException()
