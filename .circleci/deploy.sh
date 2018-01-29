55b2790eaeb4b0249140095006b0b90d03d309a2
Jeff Ashton <JeffAshton@users.noreply.github.com>
2018-01-29 12:14:27 -0500

    Always appending build number

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,4 +2,6 @@
 set -eu
 
 docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
-docker push "$DOCKER_IMAGE_NAME"
+
+docker tag "$DOCKER_IMAGE_NAME" "$DOCKER_IMAGE_NAME-$CIRCLE_BUILD_NUM"
+docker push "$DOCKER_IMAGE_NAME-$CIRCLE_BUILD_NUM"
