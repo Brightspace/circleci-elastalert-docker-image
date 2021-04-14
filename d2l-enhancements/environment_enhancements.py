170cb29b725ec17ee1c1b65c3d5ff91679910f69
Jeff Ashton <JeffAshton@users.noreply.github.com>
2021-04-14 10:58:43 -0400

    Adding OnlyDevelopmentEnvironments enhancement

--- d2l-enhancements/environment_enhancements.py
+++ d2l-enhancements/environment_enhancements.py
@@ -3,7 +3,6 @@ from elastalert.enhancements import DropMatchException
 from os import environ
 
 class AppendDataCenter(BaseEnhancement):
-
 	def process(self, match):
 		if 'D2L_DATA_CENTER' in environ:
 			match['d2l_data_center'] = environ['D2L_DATA_CENTER']
@@ -11,8 +10,9 @@ class AppendDataCenter(BaseEnhancement):
 			match['d2l_data_center'] = 'unknown'
 
 class ExcludeDevelopmentEnvironments(BaseEnhancement):
-
 	def process(self, match):
-		if 'D2L_DEVELOPMENT_ENVIRONMENT' in environ:
-			if environ['D2L_DEVELOPMENT_ENVIRONMENT'] == 'True':
-				raise DropMatchException()
+		pass
+
+class OnlyDevelopmentEnvironments(BaseEnhancement):
+	def process(self, match):
+		pass
