d9c107fba91d3dd92e53f3edde80f2ba0ea90dae
Jeff Ashton <JeffAshton@users.noreply.github.com>
2021-06-25 13:19:10 -0400

    whitespace

--- d2l-enhancements/setup.py
+++ d2l-enhancements/setup.py
@@ -2,9 +2,9 @@
 import os
 from setuptools import setup
 
-base_dir = os.path.dirname( __file__ )
+base_dir = os.path.dirname(__file__)
 setup(
 	name = 'd2l',
-	packages = [ 'd2l' ],
+	packages = ['d2l'],
 	setup_requires = 'setuptools'
 )
